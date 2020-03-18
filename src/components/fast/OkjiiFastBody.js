b654c3e1e287d2231f94962c33fd67f69134fd61
Ritesh Kumar <ritesh.prog@gmail.com>
2020-03-19 00:14:08 +0530

    first commit

--- src/components/fast/OkjiiFastBody.js
+++ src/components/fast/OkjiiFastBody.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import img4 from '../../img/okjiifast/img4.png';
-//import img2a from '../img/okjiifast/img2a.png';
-//import img2b from '../img/okjiifast/img2b.png';
-//import img2c from '../img/okjiifast/img2c.png';
-import Howwework from '../../img/okjiifast/Howwework.png';
+import OkkjifastBackground from '../../img/okjiifast/OkkjifastBackground.png';
+import img2a from '../../img/okjiifast/img2a.png';
+import img2b from '../../img/okjiifast/img2b.png';
+import img2c from '../../img/okjiifast/img2c.png';
+//import Howwework from '../../img/okjiifast/Howwework.png';
 import OkkJiFast01 from '../../img/okjiifast/OkkJiFast01.png';
 //import img1 from '../img/okjiifast/img1.png';
+import OkjiiRapid from '../fast/OkjiiRapid';
+
 import Testimonial from '../common/testimonial/Testimonial';
 import Trusted from '../common/Trusted';
 
@@ -13,54 +15,52 @@ export default class OkjiiFastBody extends Component {
   render() {
     return (
       <div className="ok-fast">
+
       <div className="container-fluid okkji-faq">
         <div className="row">
       <figure className="okkji-faq-figure okjii-fast-img4">
-        <img src={img4} alt="" className="img-fluid " />
+        <img src={OkkjifastBackground} alt="" className="img-fluid " />
       </figure>
       <div class="okjii-fastcon">
-        <h1><b>OKKJI Fast</b></h1>
-        <h2>An exclusive OkkJi service</h2>
+        <h1>AN EXCLUSIVE OKKJI SERVICE</h1>
       
         </div>
+
+        <div className="container text-center okkji-howwework">
+        <div className="row text-center">
+        <div class="col-md-12 text-center"><h1 className="text-center">How we work</h1></div>
+        </div>      
+        <div class="row okjii-fasthow text-center">
+          <div className="col-md-4 col-sm-6">
+          <figure>
+            <img src={img2a} className="img-fluid " alt="" />
+            <figcaption><h4>Install App</h4></figcaption>
+          </figure>
+          </div>
+          <div className="col-md-4 col-sm-6">
+          <figure>
+            <img src={img2b} className="img-fluid " alt="" />
+            <figcaption><h4>Order Online</h4></figcaption>
+          </figure>
+          </div>
+          <div className="col-md-4 col-sm-6 text-center">
+          <figure>
+            <img src={img2c} className="img-fluid " alt="" />
+            <figcaption><h4>Fast Delivery</h4></figcaption>
+          </figure>
+          </div>
+          
+          </div>
+        </div>
+
         </div>
 
    </div>
 
 
-      <div className="container-fluid">
-      
-        <div class="row okjii-fasthow">
-          <div className="">
-          <h1 className="text-center">How we work</h1>
-          <figure>
-            <img src={Howwework} className="img-fluid " alt="" />
-</figure>
-</div>
-</div>
-        </div>
-        <div className="container-fluid">
-        <div class="row okjii-fastfast">
-          <div className="">
-          <h1 className="okjii-fast">OkkJi Fast</h1>
-          <figure className="OkkJiFast01">
-            <img src={OkkJiFast01} className="img-fluid " alt="" />
-          </figure>
-          </div>
-          {/*<div className="okjii-fast-left1">
-          <div className="okjii-fast-left">Rapid delivery at door step</div>
-          <div className="okjii-fast-left">Get all neighbourhood shop</div>
-          <div className="okjii-fast-left">Additional hand for shops to deliver on time</div>
-          <div className="okjii-fast-left">Relationship-oriented shopping in fast track mode</div>
-          </div>
-          <div className="okjii-fast-right1">
-          
-          <div className="okjii-fast-right">Three easy steps to shop</div>
-          <div className="okjii-fast-right">An expressway between shop and customer</div>
-          <div className="okjii-fast-right">Order anytime, anywhere from nearby store</div>
-          <div className="okjii-fast-right">Boon for shops that cannot effort home delivery</div>
-  </div>*/}
-        </div>
+        <div className="">
+          <h1 className="text-center okjii-fastheading">OKKJI Fast</h1>
+          <OkjiiRapid />
         </div>
         <Testimonial />
         <Trusted />
