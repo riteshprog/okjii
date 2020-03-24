58de028074108435bc837ed894f6d87fed08e9c3
Ritesh Kumar <ritesh.prog@gmail.com>
2020-03-24 17:58:19 +0530

    first commit

--- src/components/blog/OkkjiBlog.js
+++ src/components/blog/OkkjiBlog.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import OkkjiBlogBody from '../blog/OkkjiBlogBody';
-
+import { Helmet } from 'react-helmet';
 export default class OkkjiBlog extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+<title>Blog</title>
+</Helmet>
         <OkkjiBlogBody />
       </div>
     )
--- src/components/registrationoffer/RegistrationOffer.js
+++ src/components/registrationoffer/RegistrationOffer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import RegistrationOfferBody from '../registrationoffer/RegistrationOfferBody';
+import { Helmet } from 'react-helmet';
 
 
 export default class RegistrationOffer extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+<title>Customers offer</title>
+</Helmet>
         <RegistrationOfferBody />
       </div>
     )
