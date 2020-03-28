abc7d591c8b8ae2b7bafc5e37ecaa00f8ee79fbe
Ritesh Kumar <ritesh.prog@gmail.com>
2020-03-28 19:12:49 +0530

    asd

--- src/components/uploadselfi/UplaodSelfyBody.js
+++ src/components/uploadselfi/UplaodSelfyBody.js
@@ -1,29 +1,39 @@
-import React, { Component } from 'react'
-import logo192 from "../../img/home/logo192.png";
+import React, { Component } from 'react';
+import okjiilogo from "../../img/selfy/okjiilogo.png";
+import Doctor from "../../img/selfy/Doctor.png";
+import Police from "../../img/selfy/Police.png";
+import DeliveryPerson from "../../img/selfy/DeliveryPerson.png";
 
 export default class UplaodSelfyBody extends Component {
   render() {
     return (
       <div className="blog-page">
         <div className="container selfi-blog">
-          <div className="blog">
-          <div className="left"><p>We Care 24 Hour for You</p>
-          <img src={logo192} alt="" className="img-fluid selfi-blogicon" />
+
+          <div className="row blog">
+            <div className="col-md-6">
+          <div className="left1"><p>We Care 24 Hour for You</p>
+          <img src={Doctor} alt="" className="img-fluid selfi-blogicon" />
 </div>
-          <div className="right"><p>We Protect 24 Hour For You</p>
-          <img src={logo192} alt="" className="img-fluid selfi-blogicon" />
 </div>
-          </div>
-          <div className="blog">
-          <div className="left"><p>We Surve 24 Hour For You</p>
-          <img src={logo192} alt="" className="img-fluid selfi-blogicon" />
+<div className="col-md-6">
+          <div className="right1"><p>We Protect 24 Hour For You</p>
+          <img src={Police} alt="" className="img-fluid selfi-blogicon" />
 </div>
-          <div className="right"><p>I Stay At Home 24 Hour For all of You</p>
+</div>
+          <div className="col-md-6">
+          <div className="left1"><p>We Surve 24 Hour For You</p>
+          <img src={DeliveryPerson} alt="" className="img-fluid selfi-blogicon" />
+</div>
+</div>
+<div className="col-md-6">
+          <div className="right1"><p>I Stay At Home 24 Hour For all of You</p>
           <p><button  type="file">Click Your Image And<br />Share On Social Media</button></p></div>
           </div>
+          </div>
+          <img src={okjiilogo} alt="" className="img-fluid selfi-icon" />
         </div>
 
-        <img src={logo192} alt="" className="img-fluid selfi-icon" />
       </div>
     )
   }
