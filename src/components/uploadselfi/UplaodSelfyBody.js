86c57b72c5dcc874576720e5207c704d3666ab35
Ritesh Kumar <ritesh.prog@gmail.com>
2020-03-29 11:41:50 +0530

    asd

--- src/components/uploadselfi/UplaodSelfyBody.js
+++ src/components/uploadselfi/UplaodSelfyBody.js
@@ -8,25 +8,28 @@ export default class UplaodSelfyBody extends Component {
   render() {
     return (
       <div className="blog-page">
-        <div className="container selfi-blog">
+        <div className="selfi-blog">
 
-          <div className="row blog">
-            <div className="col-md-6">
+          <div className=" blog">
+            <div className="">
           <div className="left1"><p>We Care 24 Hour for You</p>
           <img src={Doctor} alt="" className="img-fluid selfi-blogicon" />
 </div>
 </div>
-<div className="col-md-6">
-          <div className="right1"><p>We Protect 24 Hour For You</p>
-          <img src={Police} alt="" className="img-fluid selfi-blogicon" />
-</div>
-</div>
-          <div className="col-md-6">
+<div className="">
           <div className="left1"><p>We Surve 24 Hour For You</p>
           <img src={DeliveryPerson} alt="" className="img-fluid selfi-blogicon" />
 </div>
 </div>
-<div className="col-md-6">
+</div>
+<div className="blog">
+<div className="">
+          <div className="right1"><p>We Protect 24 Hour For You</p>
+          <img src={Police} alt="" className="img-fluid selfi-blogicon" />
+</div>
+</div>
+      
+<div className="">
           <div className="right1"><p>I Stay At Home 24 Hour For all of You</p>
           <p><button  type="file">Click Your Image And<br />Share On Social Media</button></p></div>
           </div>
