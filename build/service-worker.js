24bf84ce4c5aceb43ee77e09734faaf000b3dd80
Ravi Kant Singh <raysk7161@gmail.com>
2020-04-15 03:14:53 +0530

    added build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aaa0162385689fbdab5bc3b8613d4e08.js"
+  "/precache-manifest.eb1f87abdb8bd9c3ac6dc102582abfda.js"
 );
 
 self.addEventListener('message', (event) => {
