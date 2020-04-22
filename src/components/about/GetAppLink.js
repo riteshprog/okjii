41ea91cf4a6a4555abaf5361c56c505c2cf53077
nirmalkukna1999 <nirmal.kukna@dianapps.com>
2020-04-22 14:51:47 +0530

    home page update and header update

--- src/components/about/GetAppLink.js
+++ src/components/about/GetAppLink.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { MDBInputGroup } from "mdbreact";
+import AppleIcon from "../../img/home/Appleicon.png";
+import PlayStore from "../../img/home/Playstore.png";
 
 const GetAppLink = () => {
   return (
+
     <div className="getapp-link ">
-      <MDBInputGroup
-        style={{ height: '100%', padding: 'none'}}
-        container
-        ClassName="mb-3"
-        prepend="+91 "
-        append="GET APP LINK"
-        hint="Mobile Number"
-        type="number"
-        size="lg"
-      />
+      <div className="row">
+        <MDBInputGroup
+          style={{ height: '100%',width: '100%', padding: 'none'}}
+          container
+          ClassName="mb-3"
+          prepend="+91"
+          append="GET APP LINK"
+          hint="Mobile Number"
+          type="number"
+          size="lg"
+        />
+      </div>
+      <div className="appIcon-row">
+        <img src={AppleIcon} className="img-fluid appIcon" />
+        <img src={PlayStore} className="img-fluid appIcon" />
+      </div>
     </div>
   );
 };
--- src/components/home/mainbody.js
+++ src/components/home/mainbody.js
@@ -8,15 +8,16 @@ import Asset6 from "../../img/home/Asset6.png";
 import apple from "../../img/home/apple.png";
 
 import google from "../../img/home/google.png";
-
+import HomeVideo from "../../img/home/Home4.mp4";
 import Asset7 from "../../img/home/Asset7.png";
+import RetailerHome from "../../img/home/RetailerHome-01.png";
 
 import HomeDelivery from "./HomeDelivery";
 
 import KhataBook from "./KhataBook";
 
 import Testimonial from "../common/testimonial/Testimonial";
-
+import CustomerImage from "../../img/home/customericon.png"
 import Trusted from "../common/Trusted";
 import GetAppLink from "../about/GetAppLink";
 import { Helmet } from "react-helmet";
@@ -25,92 +26,115 @@ export default class mainbody extends Component {
   render() {
     return (
       <div className="okjii-home">
-        <Helmet>
-          <title>Home | Online Kirana Shopping | Near By Shop</title>
-        </Helmet>
-        <div className="container-fluid okjii-mainbody main-body-desktop">
-          <div className="row image-position">
-            <div className="col-lg-7 col-md-7 order-lg-12 image-row">
-              <img src={Asset6} className="img-fluid Asset6" alt="" />
-            </div>
-            <div className="col-lg-5 col-md-5 order-lg-1 okjii-explore okjii-explore-left">
-              <h1>EASIEST WAY TO ORDER</h1>
+        <div className="okkjii-home-bg">
+          <Helmet>
+            <title>Home | Online Kirana Shopping | Near By Shop</title>
+          </Helmet>
+          <div className="container-fluid okjii-mainbody main-body-desktop">
+            <div className="row image-position">
 
-              <h2>from your local kirana shops.</h2>
+              <div className="col-lg-5 col-md-5 order-lg-12">
 
-              <p>
-                A digital hub of all neighbourhood retail shops with new age
-                look and a simple setup.
-              </p>
-              <div className="row">
-                <GetAppLink />
-              </div>
+                <div className="home-video">
+                <video src={HomeVideo} preload="auto"
+                   autoplay="true"
+                   muted="true"
+                   loop="true"
+                   ></video>
 
-              <div className="okjii-install-image">
-                <div className="row ">
-                  <div className="col-sm-12  okjii-install">
-                    <p className="install-now text-center ">INSTALL NOW</p>
-
-                    <p className="text-center">
-                      <img src={Asset7} alt="" />
-                    </p>
-                  </div>
+                </div>
+                <div className="col-lg-10 getapplink-ml">
+                  <GetAppLink />
                 </div>
 
-                <div className="row  text-center">
-                  <div className="col-sm-6">
-                    <img className="img-fluid apple" src={apple} alt="" />
+              </div>
+              <div className="col-lg-6 col-md-6 order-lg-1 okjii-explore-left">
+                <div className="okjii-explore">
+                  <h1>Best way to explore</h1>
+
+                  <h2>all nearby mom-dad retail shops.</h2>
+                </div>
+
+                <div className="home-page-order">
+                  <img src={RetailerHome} className="img-fluid RetailerHome" alt="" />
+                </div>
+                <div className="col-lg-8 col-md-9 home-order-details">
+                  <div className="total-order-amount row">
+                      <div className="col-lg-6 col-md-6">
+                        <p>Total Order</p>
+                        <p className="order-color">14 Orders</p>
+                      </div>
+                      <div className="col-lg-6 col-md-6 left-order-border">
+                        <p>Order Amount</p>
+                        <p className="order-amount-cl">Rs. 765</p>
+                      </div>
                   </div>
-                  <div className="col-sm-6">
-                    <img className="img-fluid google" src={google} alt="" />
+                  <div className="recent-order-list">
+                    <div className="order-list-items row">
+                      <img src={CustomerImage} className="img-fluid CustomerImage" alt="" />
+                      <p>Sumit Kumar Singh</p>
+                      <p className="pright">Order of <span>Rs. 770</span></p>
+                    </div>
+                    <div className="order-list-items order-mt1 row">
+                      <img src={CustomerImage} className="img-fluid CustomerImage" alt="" />
+                      <p>Sumit Kumar Singh</p>
+                      <p className="pright">Order of <span>Rs. 770</span></p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
+
+
+
         {/* Mobile View */}
 
         <div className="container-fluid okjii-mainbody-mobile main-body-mobile">
-          <div className="row okjii-explore okjii-explore-left">
-            <h1>EASIEST WAY TO ORDER</h1>
+          <div className="okjii-explore okjii-explore-left">
+            <h1>Best way to explore</h1>
 
-            <h2>from your local kirana shops.</h2>
-
-            <p>
-              A digital hub of all neighbourhood retail shops with new age look
-              and a simple setup.
-            </p>
-            <div className="row offset-lg-1 offset-md-1">
+              <h2>all nearby mom-dad retail shops.</h2>
+              <div className="home-page-order">
+                <img src={RetailerHome} className="img-fluid RetailerHome" alt="" />
+              </div>
+              <div className="col-lg-8 col-md-9 home-order-details">
+                <div className="total-order-amount row">
+                    <div className="col-6 col-lg-6 col-md-6">
+                      <p>Total Order</p>
+                      <p className="order-color">14 Orders</p>
+                    </div>
+                    <div className="col-6 col-lg-6 col-md-6 left-order-border">
+                      <p>Order Amount</p>
+                      <p className="order-amount-cl">Rs. 765</p>
+                    </div>
+                </div>
+                <div className="recent-order-list">
+                  <div className="order-list-items row">
+                    <img src={CustomerImage} className="img-fluid CustomerImage" alt="" />
+                    <p>Sumit Kumar Singh</p>
+                    <p className="pright">Order of <span>Rs. 770</span></p>
+                  </div>
+                  <div className="order-list-items order-mt1 row">
+                    <img src={CustomerImage} className="img-fluid CustomerImage" alt="" />
+                    <p>Sumit Kumar Singh</p>
+                    <p className="pright">Order of <span>Rs. 770</span></p>
+                  </div>
+                </div>
+              </div>
+            <div className="row offset-lg-1 offset-md-1 mobile-number-mt">
               <GetAppLink />
-            </div>
-
-            <div className="mobile-google text-center">
-              <div className="row text-center">
-                <div className="col-sm-6 col-md-6">
-                  {/* <img className="img-fluid apple"  src={apple} alt=""/> */}
-                </div>
-                <div className="col-sm-6 col-md-6">
-                  <img className="img-fluid google" src={google} alt="" />
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-sm-12  okjii-install">
-                  <p className="install-now text-center">
-                    <img src={Asset7} alt="" />
-                  </p>
-                  <p className=" text-center ">INSTALL NOW</p>
-                </div>
-              </div>
             </div>
           </div>
         </div>
 
-        <div className="container-fluid">
+        <div className="container-fluid okkjii-customer-mt">
           <div className="row">
             <div className="col-lg-12 col-sm-12 col-md-12 okjii-best">
               <h1 className="text-center">
-                WHY CUSTOMERS <b className="best">LOVE OKKJI?</b>
+                WHY CUSTOMERS <b className="best">LOVE OKKJI</b>
               </h1>
             </div>
           </div>
